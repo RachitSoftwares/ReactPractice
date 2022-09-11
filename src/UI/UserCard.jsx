52b1ea128d13726272c6175bbc5f0d61750529a0
AshwiniBallak <ashwiniballak@gmail.com>
2022-09-11 15:39:27 +0530

    remove cooment from user-card

--- src/UI/UserCard.jsx
+++ src/UI/UserCard.jsx
@@ -1,8 +1,15 @@
 const UserCard = (props) => {
-  console.log("Props:", props);
   const { id, email, first_name, last_name, avatar } = props.user;
   return (
-    <div style={{ width: "30%", border: "1px solid black" ,borderRadius:'10px',alignItems:'center',justifyContent:'center'}}>
+    <div
+      style={{
+        width: "30%",
+        border: "1px solid black",
+        borderRadius: "10px",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <div>
         <img src={avatar} />
       </div>
